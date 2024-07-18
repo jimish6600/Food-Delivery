2e4341f0b32010ebd557231cadb10043db4868f4
Jimish Patel <jimishpately50@gmail.com>
2024-07-18 18:26:20 +0530

    done cart page

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -18,12 +18,29 @@ const StoreContextProvider = (props) =>{
         setCartItem((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
+    const deleteFromCart = (itemId) => {
+        setCartItem((prev)=>({...prev,[itemId]:0}))
+    }
+
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo = food_list.find((product)=>product._id===item);
+                totalAmount +=itemInfo.price* cartItems[item]
+            }
+        }
+        return totalAmount;
+    } 
+
     const contextValue = {
         food_list,
         cartItems,
         setCartItem,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        deleteFromCart,
+        getTotalCartAmount
     }
 
     return (
